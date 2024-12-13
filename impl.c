f3980b668dd8ffc5afb1a4b6dd82536b9c603879
jayvesmir <jay@vesmir.com>
2024-12-13 19:47:57 +0100

    comment modules that dont compile without setup out

--- impl.c
+++ impl.c
@@ -2,40 +2,40 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #define STB_C_LEXER_IMPLEMENTATION
-#define STB_CONNECTED_COMPONENTS_IMPLEMENTATION
+// #define STB_CONNECTED_COMPONENTS_IMPLEMENTATION
 #define STB_DIVIDE_IMPLEMENTATION
 #define STB_DS_IMPLEMENTATION
 #define STB_DXT_IMPLEMENTATION
 #define STB_EASY_FONT_IMPLEMENTATION
-#define STB_HERRINGBONE_WANG_TILE_IMPLEMENTATION
+// #define STB_HERRINGBONE_WANG_TILE_IMPLEMENTATION
 #define STB_HEXWAVE_IMPLEMENTATION
-#define STB_INCLUDE_IMPLEMENTATION
+// #define STB_INCLUDE_IMPLEMENTATION
 #define STB_LEAKCHECK_IMPLEMENTATION
 #define STB_PERLIN_IMPLEMENTATION
 #define STB_RECT_PACK_IMPLEMENTATION
 #define STB_SPRINTF_IMPLEMENTATION
-#define STB_TEXTEDIT_IMPLEMENTATION
-#define STB_TILEMAP_EDITOR_IMPLEMENTATION
+// #define STB_TEXTEDIT_IMPLEMENTATION
+// #define STB_TILEMAP_EDITOR_IMPLEMENTATION
 #define STB_TRUETYPE_IMPLEMENTATION
-#define STB_VOXEL_RENDER_IMPLEMENTATION
+// #define STB_VOXEL_RENDER_IMPLEMENTATION
 
 #include "stb_image.h"
 #include "stb_image_resize2.h"
 #include "stb_image_write.h"
 #include "stb_c_lexer.h"
-#include "stb_connected_components.h"
+// #include "stb_connected_components.h"
 #include "stb_divide.h"
 #include "stb_ds.h"
 #include "stb_dxt.h"
 #include "stb_easy_font.h"
-#include "stb_herringbone_wang_tile.h"
+// #include "stb_herringbone_wang_tile.h"
 #include "stb_hexwave.h"
-#include "stb_include.h"
+// #include "stb_include.h"
 #include "stb_leakcheck.h"
 #include "stb_perlin.h"
 #include "stb_rect_pack.h"
 #include "stb_sprintf.h"
-#include "stb_textedit.h"
-#include "stb_tilemap_editor.h"
+// #include "stb_textedit.h"
+// #include "stb_tilemap_editor.h"
 #include "stb_truetype.h"
-#include "stb_voxel_render.h"
+// #include "stb_voxel_render.h"
